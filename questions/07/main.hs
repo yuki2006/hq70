708e0fe9cd7e596a5f6c55036200a799463d8a20
yuki2006 <yagfair@gmail.com>
2016-06-09 21:41:53 +0900

    remove 全角スペース

--- questions/07/main.hs
+++ questions/07/main.hs
@@ -9,8 +9,8 @@ toBinary value
 isPalindrome :: String -> Bool
 isPalindrome value = value == reverse value
 
-joinDay　:: (Int,Int,Int) -> Int
-joinDay　 (x,y,z) = x*10000 + y*100 + z
+joinDay :: (Int,Int,Int) -> Int
+joinDay (x,y,z) = x*10000 + y*100 + z
 
 
 check :: (Int,Int,Int) -> Bool
@@ -18,7 +18,7 @@ check (a,b,c)
   | otherwise = True
 main = do
    
-   putStrLn $ show $ filter (isPalindrome　. toBinary) $ map joinDay　$ filter check [(yy,mm,dd)| yy <- [1964..2020],mm <- [1..12],dd <- [1..31] ]
+   putStrLn $ show $ filter (isPalindrome . toBinary) $ map joinDay $ filter check [(yy,mm,dd)| yy <- [1964..2020],mm <- [1..12],dd <- [1..31] ]
 
 
--- YYYYMMDD　→　2進数　→　逆順にして　→　10進数　→同じ日付のものを探す
+-- YYYYMMDD → 2進数 → 逆順にして → 10進数 →同じ日付のものを探す
