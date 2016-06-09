cc0f27f8ff988ac9f3cb5dd10b630f0e2c544e0f
yuki2006 <yagfair@gmail.com>
2016-06-09 21:37:25 +0900

    solved Q07

---
+++ questions/07/main.hs
@@ -0,0 +1,24 @@
+module Main where
+
+
+toBinary :: Int -> String
+toBinary value
+  | value == 0  = ""
+  | otherwise   = (toBinary $ value `div` 2) ++ (show $ value `mod` 2)
+
+isPalindrome :: String -> Bool
+isPalindrome value = value == reverse value
+
+joinDay　:: (Int,Int,Int) -> Int
+joinDay　 (x,y,z) = x*10000 + y*100 + z
+
+
+check :: (Int,Int,Int) -> Bool
+check (a,b,c)
+  | otherwise = True
+main = do
+   
+   putStrLn $ show $ filter (isPalindrome　. toBinary) $ map joinDay　$ filter check [(yy,mm,dd)| yy <- [1964..2020],mm <- [1..12],dd <- [1..31] ]
+
+
+-- YYYYMMDD　→　2進数　→　逆順にして　→　10進数　→同じ日付のものを探す
