cfced14840ea55c257a90a578abb19b43633b668
yuki2006 <yagfair@gmail.com>
2016-10-24 21:59:17 +0900

    いらないコードの除去

--- questions/26/main.hs
+++ questions/26/main.hs
@@ -3,14 +3,17 @@ import Data.Monoid
 
 type Pos = (Int,Int)
 
+find_next:: Pos -> [Pos]
+find_next (x,y) 
+
 bfs:: (Pos,Pos) -> [Pos] 
 bfs p = bfs_aux [x:[p] | x<-find_next p]
 bfs_aux (path:queue)
     | x==1 && y==1 = (return (reverse path)) `mplus` (bfs_aux (1,1) queue)
     | otherwise = bfs_aux (1,1) (queue ++ [x:path|x<-find_next p,not (x `elem` path)])
- where (x,y) = head path
+ where ((x,y),_) = head path
 
 main :: IO()
-main = print $ getSum $ pitchM allShots
+main = print "Q26"
 
 
